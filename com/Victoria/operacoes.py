8e350fe4941ab513c5630db5a74d6145589aca28
viholv7 <61299149+viholv7@users.noreply.github.com>
2020-04-03 20:20:46 -0300

    atualização

---
+++ com/Victoria/operacoes.py
@@ -0,0 +1,9 @@
+classe  Operacoes ():
+    def  soma ( auto , Valores ):
+        val  =  0
+        para  v  em  valores :
+            val  =  val  +  v
+        retorno  val
+
+# print (Operações (). soma ([1, 3]))
+© 2020 GitHub, Inc.
